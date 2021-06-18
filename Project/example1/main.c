4f18e4c8bbd6c58c61acedf0530771dafe199629
yangqingxian <qingxianyang@gmail.com>
2021-06-18 03:32:05 -0700

    尝试一些工程之类的内容

---
+++ Project/example1/main.c
@@ -0,0 +1,8 @@
+#include "functions.h"
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    printf("getNumber() return is %d\n",getNumber());
+    return 0;
+}
---
+++ Project/example1/src/functions.c
@@ -0,0 +1,5 @@
+#include "functions.h"
+
+int getNumber(void){
+    return 1;
+}
